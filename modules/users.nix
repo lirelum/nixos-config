aba03ae16607a85adde3a9523ff90c43b8cc035d
Autumn <lirelum@disroot.org>
2026-08-15 00:47:12 -0400

    Refactor users slightly

--- modules/users.nix
+++ modules/users.nix
@@ -9,29 +9,30 @@
   options = {
     my.users.autumn.enable = lib.mkEnableOption "user autumn";
   };
-  config = {
-    users.users.autumn = lib.mkIf config.my.users.autumn.enable {
-      isNormalUser = true;
+  config = lib.mkMerge [
+    (lib.mkIf config.my.users.autumn.enable {
+      users.users.autumn = {
+        isNormalUser = true;
 
-      description = "Autumn";
+        description = "Autumn";
 
-      extraGroups = [
-        "wheel"
-        "networkmanager"
-        "video"
-        "audio"
-      ];
+        extraGroups = [
+          "wheel"
+          "networkmanager"
+          "video"
+          "audio"
+        ];
 
-      shell = pkgs.zsh;
-    };
-    programs.zsh.enable = lib.mkIf config.my.users.autumn.enable true;
-
-    home-manager = lib.mkIf config.my.home.enable {
-      users.autumn = lib.mkIf config.my.users.autumn.enable {
-        imports = builtins.attrValues outputs.homeModules;
-
-        programs.zsh.enable = true;
+        shell = pkgs.zsh;
       };
-    };
-  };
+      programs.zsh.enable = true;
+
+      home-manager = lib.mkIf config.my.home.enable {
+        users.autumn = {
+          imports = builtins.attrValues outputs.homeModules;
+          programs.zsh.enable = true;
+        };
+      };
+    })
+  ];
 }
