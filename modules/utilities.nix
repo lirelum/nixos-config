0e006c54ce09c4448845a99423643864fa7a2724
Autumn <lirelum@disroot.org>
2026-08-15 06:57:43 -0400

    Add nix-ld for running external programs

--- modules/utilities.nix
+++ modules/utilities.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
   environment.systemPackages = with pkgs; [
     vim
     eza
@@ -15,4 +15,36 @@
     lsof
     lshw
   ];
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      # Base C/C++ runtime & system dependencies
+      stdenv.cc.cc.lib
+      zlib
+      glibc
+      util-linux # libuuid
+
+      # Security & Networking
+      openssl
+      curl
+      libkrb5
+
+      # Desktop & Graphics (OpenGL/Vulkan for renderers/GUI extensions)
+      libGL
+      xorg.libX11
+      xorg.libXcursor
+      xorg.libXrandr
+      xorg.libXi
+      wayland
+
+      # Internationalization & Encoding
+      icu
+      libxml2
+
+      # Audio & System Interoperability
+      alsa-lib
+      fontconfig
+      freetype
+    ];
+  };
 }
