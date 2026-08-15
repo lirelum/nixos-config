65719435ffe5f3248feaa2699fef88b738c137f6
Autumn <lirelum@disroot.org>
2026-08-15 06:02:24 -0400

    Add i18n settings

---
+++ modules/i18n.nix
@@ -0,0 +1,12 @@
+{pkgs, ...}: {
+  i18n.defaultLocale = "ja_JP.UTF-8";
+
+  i18n.supportedLocales = [ "en_US.UTF-8/UTF-8" "fr_CA.UTF-8/UTF-8" "ja_JP.UTF-8/UTF-8" ];
+
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-color-emoji
+  ];
+}
