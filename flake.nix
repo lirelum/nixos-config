ec70992cee6f973efcd119866c4de3e235b82b16
Autumn <lirelum@disroot.org>
2026-08-15 01:08:28 -0400

    Fix version mismatch with home-manager

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager?ref=release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
