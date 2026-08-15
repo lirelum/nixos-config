5d0d68f9f97510e08b1cd8750bfdebfb874c0584
Autumn <lirelum@disroot.org>
2026-08-15 05:41:54 -0400

    Add gaming relevant settings

---
+++ modules/gaming.nix
@@ -0,0 +1,11 @@
+{
+  my.gui.enable = true;
+  programs.steam = {
+    enable = true;
+    gamescopeSession.enable = true;
+  };
+
+  programs.gamemode.enable = true;
+
+  programs.gamescope.enable = true;
+}
--- modules/gui.nix
+++ modules/gui.nix
@@ -1,8 +1,12 @@
-{lib, config, ...}: {
+{ lib, config, ... }: {
   options.my.gui.enable = lib.mkEnableOption "gui config";
   config = lib.mkIf config.my.gui.enable {
     services.xserver = {
       enable = true;
+    };
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
     };
   };
 }
