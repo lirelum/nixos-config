6a21731d5e04e5071e5a12660b22f3dd33d64318
Autumn <lirelum@disroot.org>
2026-08-16 01:05:32 -0400

    Add extensions and ancillary programs for gnome

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -1,8 +1,19 @@
-{ lib, config, ... }: {
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
   options.my.gnome.enable = lib.mkEnableOption "gnome desktop and gdm";
   config = lib.mkIf config.my.gnome.enable {
     my.gui.enable = true;
     services.displayManager.gdm.enable = true;
     services.desktopManager.gnome.enable = true;
+    environment.systemPackages = with pkgs; [
+      gnome-tweaks
+      gnomeExtensions.fullscreen-avoider
+      gnomeExtensions.appindicator
+    ];
   };
 }
