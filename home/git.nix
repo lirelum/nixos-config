10fe78589bf252415a2ca09574992e85aa02fb3c
Autumn <lirelum@disroot.org>
2026-08-16 00:27:53 -0400

    Git stuff

--- home/git.nix
+++ home/git.nix
@@ -1,5 +1,12 @@
 {
   programs.git = {
     enable = true;
+    settings = {
+      user = {
+        name = "Autumn";
+        email = "lirelum@disroot.org";
+      };
+      init.defaultBranch = "main";
+    };
   };
 }
