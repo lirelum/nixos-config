312de6b5d66f0847b269304cd67b5e2b30f65cce
Autumn <lirelum@disroot.org>
2026-08-21 17:58:37 -0400

    Refactor shell aliases to use references to the nix store rather than relying on another module pulling in the correct programs, and add aliases to use eza in place of ls

--- home/zsh.nix
+++ home/zsh.nix
@@ -1,14 +1,15 @@
-{
+{ pkgs, lib, ... }: {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     shellAliases = {
-      ls = "ls --color=auto";
-      ll = "ls -la";
-      la = "ls -a";
-      nsw = "sudo nixos-rebuild switch --flake ~/.config/nixos/";
+      ls = "${lib.getExe pkgs.eza}";
+      la = "${lib.getExe pkgs.eza} -a";
+      ll = "${lib.getExe pkgs.eza} -la";
+      lt = "${lib.getExe pkgs.eza} --tree";
+      nsw = "sudo ${lib.getExe pkgs.nixos-rebuild} switch --flake ~/.config/nixos/";
     };
   };
 }
