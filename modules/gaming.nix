05b9117a84acd2a378527b0b31782da9baa53e6c
Autumn <lirelum@disroot.org>
2026-08-30 12:47:19 -0400

    Add proton-ge for steam

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -1,6 +1,11 @@
-{
+{ pkgs, ... }: {
   imports = [ ./gui.nix ];
-  programs.steam.enable = true;
+  programs.steam = {
+    enable = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
+  };
   programs.gamemode.enable = true;
   programs.gamescope.enable = true;
 }
